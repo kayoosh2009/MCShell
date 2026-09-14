d7811adb441896e0347dc419ad798e6946498403
kayoosh2009 <kayoosh2009@gmail.com>
2026-09-14 18:26:46 +0300

    1.0.1 (24)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,30 +8,32 @@ fn launch_game(nickname: String, version: String, game_dir: String, java_path: S
         return Err("Введите никнейм!".into());
     }
 
-    // Путь к JAR-файлу версии (например, .minecraft/versions/1.20.1/1.20.1.jar)
-    let version_jar = format!("{}/versions/{}/{}.jar", game_dir, version, version);
+    // Раскрываем символ ~ в домашнюю директорию, если пользователь передал путь с ним
+    let expanded_dir = if game_dir.starts_with("~") {
+        let home = std::env::var("HOME").unwrap_or_default();
+        game_dir.replacen("~", &home, 1)
+    } else {
+        game_dir
+    };
 
-    // В полноценном лаунчере здесь собирается список всех .jar из папки libraries.
-    // Для базового PoC передаем основной клиентский JAR:
+    let version_jar = format!("{}/versions/{}/{}.jar", expanded_dir, version, version);
     let classpath = version_jar;
 
-    // Формируем аргументы запуска Minecraft
     let args = vec![
-        "-Xmx2G".to_string(), // Выделяем 2 ГБ ОЗУ
-        format!("-Djava.library.path={}/versions/{}/natives", game_dir, version),
+        "-Xmx2G".to_string(),
+        format!("-Djava.library.path={}/versions/{}/natives", expanded_dir, version),
         "-cp".to_string(),
         classpath,
-        "net.minecraft.client.main.Main".to_string(), // Главный класс игры
+        "net.minecraft.client.main.Main".to_string(),
         "--username".to_string(), nickname,
         "--version".to_string(), version,
-        "--gameDir".to_string(), game_dir,
-        "--assetsDir".to_string(), format!("{}/assets", game_dir),
+        "--gameDir".to_string(), expanded_dir.clone(),
+        "--assetsDir".to_string(), format!("{}/assets", expanded_dir),
         "--assetIndex".to_string(), version,
-        "--accessToken".to_string(), "0".to_string(), // Токен для офлайн-режима
+        "--accessToken".to_string(), "0".to_string(),
         "--userProperties".to_string(), "{}".to_string(),
     ];
 
-    // Запускаем процесс Minecraft
     match Command::new(java_path).args(&args).spawn() {
         Ok(_) => Ok("Игра запускается!".into()),
         Err(e) => Err(format!("Ошибка запуска Java: {}", e)),
